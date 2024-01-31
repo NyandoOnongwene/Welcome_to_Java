da12bc4b7f341cbe09145cd10be1a36490f55aca
Nyando Onongwene <nyandoonotex@gmail.com>
2024-01-31 22:41:50 +0100

    Another commit

--- src/main/java/org/example/Welcome.java
+++ src/main/java/org/example/Welcome.java
@@ -3,5 +3,6 @@ package org.example;
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Welcome\nto\nJava\nProgramming!");
     }
 }
