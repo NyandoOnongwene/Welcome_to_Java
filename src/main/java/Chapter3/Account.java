10d95a735de437580f0c4ac3e15f59b560b2f0f2
Nyando Onongwene <nyandoonotex@gmail.com>
2024-02-01 21:14:08 +0100

    created a class account and a instance variable

---
+++ src/main/java/Chapter3/Account.java
@@ -0,0 +1,13 @@
+package Chapter3;
+
+public class Account {
+    private String name; //instance variable
+    public void setName(String name)
+    {
+        this.name = name; //Store the name
+
+        //To retrieve the name
+        public String getName() {
+            return name;
+    }
+    }}
